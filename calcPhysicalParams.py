1f69ac9b1632b4480805138c298b1f40158e4636
Stuart Littlefair <s.littlefair@shef.ac.uk>
2019-10-04 11:29:32 +0100

    fixing calcPhysicalParams

--- calcPhysicalParams.py
+++ calcPhysicalParams.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python
 import argparse
 import os
-import sys
 from functools import partial
 
 import numpy as np
 import seaborn as sns
 from astropy import constants as const
 from astropy import units
-from astropy.table import Column, Table
+from astropy.table import Table
 from astropy.utils.console import ProgressBar as PB
+from scipy import interpolate as interp
+from scipy.optimize import brentq
+from trm import roche
+from mcmc_utils import readchain_dask, readflatchain, flatchain
 
 # see if our astropy version supports quantities or not
 quantitySupport = True
@@ -22,82 +25,85 @@ except:
 def read_wood_file(filename):
     '''The Wood 1995 thick H layer models (CO)
         returns temp and radius in K and solar units'''
-    x,y = np.loadtxt(filename,usecols=(5,6)).T
+    x, y = np.loadtxt(filename, usecols=(5, 6)).T
     temp = 10**y * units.K
     radius = 10**x * units.R_sun / const.R_sun.to(units.cm).value
     # must reverse so temp increases...
-    return temp[::-1],radius[::-1]
+    return temp[::-1], radius[::-1]
+
 
 def read_panei_file(filename):
-    x,y = np.loadtxt(filename,usecols=(1,2)).T
+    x, y = np.loadtxt(filename, usecols=(1, 2)).T
     temp = 10**y * units.K
     radius = 10**x * units.R_sun / const.R_sun.to(units.cm).value
-    temp,mass,radius= np.loadtxt(filename).T
+    temp, mass, radius = np.loadtxt(filename).T
     temp = 1000*temp*units.K
     mass = mass*units.M_sun
     radius = radius*units.R_sun/100
-    return temp,mass,radius
+    return temp, mass, radius
+
 
 def hamada_mr(baseDir):
     '''Hamada-Salpeter 0 Kelvin Mass-radius relation'''
-    filename = os.path.join(baseDir,'Hamada/mr_hamada.dat')
-    mass,radius = np.loadtxt(filename,usecols=(0,5)).T
+    filename = os.path.join(baseDir, 'Hamada/mr_hamada.dat')
+    mass, radius = np.loadtxt(filename, usecols=(0, 5)).T
     # sort out units
-    mass,radius = mass*units.M_sun,radius*units.R_sun/100.0
+    mass, radius = mass*units.M_sun, radius*units.R_sun/100.0
     # return function which interpolates these points linearly
-    return interp.interp1d(mass,radius,kind='linear')
+    return interp.interp1d(mass, radius, kind='linear')
 
-def panei_mr(targetTemp,baseDir):
+
+def panei_mr(targetTemp, baseDir):
     '''given a target temp, returns a function giving
     radius as a function of mass.
     function is derived from cubic interpolation of Panei models'''
-    assert np.all((targetTemp>=5000*units.K)&(targetTemp<45000*units.K)), \
+    assert np.all((targetTemp >= 5000*units.K) & (targetTemp < 45000*units.K)), \
         "Model invalid at temps less than 4000 or greater than 45,000 K"
 
     # read panei model grid in
-    teffs,masses,radii = \
-        read_panei_file(os.path.join(baseDir,'Panei/12C-MR-H-He/12C-MR-H-He.dat'))
+    teffs, masses, radii = \
+        read_panei_file(os.path.join(baseDir, 'Panei/12C-MR-H-He/12C-MR-H-He.dat'))
 
     # this function interpolates to give radius as function of temp, mass
-    func = interp.SmoothBivariateSpline(teffs,masses,radii)
+    func = interp.SmoothBivariateSpline(teffs, masses, radii)
 
     # this function specifies the target temp to give just radius as fn of mass
-    f2 = lambda x: func(targetTemp,x)[0]
+    f2 = lambda x: func(targetTemp, x)[0]
     return f2
 
-def wood_mr(targetTemp,baseDir):
+
+def wood_mr(targetTemp, baseDir):
     '''given a target temp, returns a function giving
         radius as a function of mass.
 
         function is derived from cubic interpolation of wood models'''
-    files = ['Wood95/co040.2.04dt1', \
-     'Wood95/co050.2.04dt1', \
-     'Wood95/co060.2.04dt1', \
-     'Wood95/co070.2.04dt1', \
-     'Wood95/co080.2.04dt1', \
-     'Wood95/co090.2.04dt1', \
-     'Wood95/co100.2.04dt1']
-    m = np.array([0.4,0.5,0.6,0.7,0.8,0.9,1.0]) * units.M_sun
+    files = ['Wood95/co040.2.04dt1', 'Wood95/co050.2.04dt1',
+             'Wood95/co060.2.04dt1', 'Wood95/co070.2.04dt1',
+             'Wood95/co080.2.04dt1',  'Wood95/co090.2.04dt1',
+             'Wood95/co100.2.04dt1']
+    m = np.array([0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]) * units.M_sun
     r = []
     for file in files:
-        temp,rad = read_wood_file( os.path.join(baseDir,file) )
+        temp, rad = read_wood_file(os.path.join(baseDir, file))
         # linear interpolation to find radius at this mass, temp
-        r.append(np.interp(targetTemp.value,temp,rad))
+        r.append(np.interp(targetTemp.value, temp, rad))
 
     # fix units
     r = np.array(r)*units.R_sun
 
     # now return function which interpolates m,r arrays cubically
-    return interp.interp1d(m,r,kind='cubic')
+    return interp.interp1d(m, r, kind='cubic')
 
-def geoFunc(mtest,scaled_mass,rw_a):
+
+def geoFunc(mtest, scaled_mass, rw_a):
     '''given a scaled mass thingy and rw/a, returns a radius in solar units'''
     a3 = scaled_mass*mtest*units.M_sun
     a = a3**(1./3.)
     rtest = rw_a*a
     return rtest.to(units.R_sun).value
 
-def find_wdmass(wdtemp,scaled_mass,rw_a,baseDir,model='hamada'):
+
+def find_wdmass(wdtemp, scaled_mass, rw_a, baseDir, model='hamada'):
     '''for a given white dwarf mass we have two estimates of the white dwarf radius:
         one from WD theoretical models, and one from the scaled_mass (which gives a)
         and rw_a - this is the geometric mass.
@@ -105,25 +111,25 @@ def find_wdmass(wdtemp,scaled_mass,rw_a,baseDir,model='hamada'):
         this routine finds the white dwarf mass where these estimates agree, if
         one exists.'''
 
-    assert model in ['hamada','wood','panei'], "Model %s not recognised" % model
-    limits = { 'hamada':[0.14,1.44], \
-        'wood':[0.4,1.0], \
-        'panei':[0.4,1.2] }
+    assert model in ['hamada', 'wood', 'panei'], "Model %s not recognised" % model
+    limits = {'hamada': [0.14, 1.44],
+              'wood': [0.4, 1.0],
+              'panei': [0.4, 1.2]}
     mlo, mhi = limits[model]
 
     if model == 'hamada':
         rwdFunc = hamada_mr(baseDir)
     elif model == 'wood':
-        rwdFunc = wood_mr(wdtemp,baseDir)
+        rwdFunc = wood_mr(wdtemp, baseDir)
     else:
-        rwdFunc = panei_mr(wdtemp,baseDir)
+        rwdFunc = panei_mr(wdtemp, baseDir)
 
     # scipys brentq finds the root of a function - in this case
     # a function which returns the difference in predicted radius at
     # a given mass.
     # geoFunc gives the geometric soln for WD radius at a given mass
     # this function = 0 when two estimates agree
-    funcToSolve = lambda x:  geoFunc(x,scaled_mass,rw_a)-rwdFunc(x)
+    funcToSolve = lambda x:  geoFunc(x, scaled_mass, rw_a)-rwdFunc(x)
 
     # first we'll check that it actually changes sign between the two
     # limits of our model. Otherwise it has no root!
@@ -134,10 +140,11 @@ def find_wdmass(wdtemp,scaled_mass,rw_a,baseDir,model='hamada'):
         raise Exception('No valid solution for this model')
 
     # OK, there should be a solution: return it
-    return brentq(funcToSolve,mlo,mhi)*units.M_sun
+    return brentq(funcToSolve, mlo, mhi)*units.M_sun
 
-def solve(input_data,baseDir):
-    q,dphi,rw,twd,p = input_data
+
+def solve(input_data, baseDir):
+    q, dphi, rw, twd, p = input_data
 
     # Kepler's law gives a quantity related to wd mass and separation, a
     # scaled_mass = a**3/M_wd. I call this the scaled mass.
@@ -150,16 +157,22 @@ def solve(input_data,baseDir):
     solved = True
     try:
         # try wood models
-        mw = find_wdmass(twd,scaled_mass,rw_a,baseDir,model='wood')
-    except:
+        mw = find_wdmass(twd, scaled_mass, rw_a, baseDir, model='wood')
+    except FileNotFoundError:
+        raise
+    except Exception:
         # try panei models (usually for masses above 1 Msun)
         try:
-            mw = find_wdmass(twd,scaled_mass,rw_a,baseDir,model='panei')
-        except:
+            mw = find_wdmass(twd, scaled_mass, rw_a, baseDir, model='panei')
+        except FileNotFoundError:
+            raise
+        except Exception:
             # try hamada models (for masses less than 0.4 or more than 1.2 Msun)
             try:
-                mw=find_wdmass(twd,scaled_mass,rw_a,baseDir,model='hamada')
-            except:
+                mw = find_wdmass(twd, scaled_mass, rw_a, baseDir, model='hamada')
+            except FileNotFoundError:
+                raise
+            except Exception:
                 solved = False
 
     # do nothing if none of the models yielded a solution
@@ -174,21 +187,21 @@ def solve(input_data,baseDir):
         a = a.to(units.R_sun)
 
         # inc
-        inc = roche.findi(q,dphi)
+        inc = roche.findi(q, dphi)
         sini = np.sin(np.radians(inc))
 
         # radial velocities
         kw = (2.0*np.pi*sini*q*a/(1+q)/p).to(units.km/units.s)
         kr = kw/q
 
-        #secondary radius
-        #use from Warner 1995 (eggleton)
-        #radius of sphere with same volume as Roche Lobe...
+        # secondary radius
+        # use from Warner 1995 (eggleton)
+        # radius of sphere with same volume as Roche Lobe...
         r2 = 0.49*a*q**(2.0/3.0)
         r2 /= 0.6 * q**(2.0/3.0) + np.log(1.0+q**(1.0/3.0))
 
         # need to be a little careful here for different versions of astropy
-        data = (q,mw,rw_a*a,mr,r2,a,kw,kr,inc)
+        data = (q, mw, rw_a*a, mr, r2, a, kw, kr, inc)
         if not quantitySupport:
             data = [getval(datum) for datum in data]
 
@@ -201,16 +214,15 @@ if __name__ == "__main__":
 
     sns.set()
     parser = argparse.ArgumentParser(description='Calculate physical parameters from MCMC chain of LFIT parameters')
-    parser.add_argument('file',action='store',help='input file from MCMC run')
-    parser.add_argument('twd',action='store',type=float,help='white dwarf temperature (K)')
-    parser.add_argument('e_twd',action='store',type=float,help='error on wd temp')
-    parser.add_argument('p',action='store',type=float,help='orbital period (days)')
-    parser.add_argument('e_p',action='store',type=float,help='error on period')
-    parser.add_argument('--thin','-t',type=int,help='amount to thin MCMC chain by',default=1)
-    parser.add_argument('--nthreads','-n',type=int,help='number of threads to run',default=6)
-    parser.add_argument('--flat','-f',type=int,help='Factor of thinning if flattened chain used',default=0)
-
-    parser.add_argument('--dir','-d',help='directory with WD models',default='/Users/mmc/lfit/params')
+    parser.add_argument('file', action='store', help='input file from MCMC run')
+    parser.add_argument('twd', action='store', type=float, help='white dwarf temperature (K)')
+    parser.add_argument('e_twd', action='store', type=float, help='error on wd temp')
+    parser.add_argument('p', action='store', ype=float, help='orbital period (days)')
+    parser.add_argument('e_p', action='store', type=float, help='error on period')
+    parser.add_argument('--thin', '-t', type=int, help='amount to thin MCMC chain by', default=1)
+    parser.add_argument('--nthreads', '-n', type=int, help='number of threads to run', default=6)
+    parser.add_argument('--flat', '-f', type=int, help='Factor of thinning if flattened chain used', default=0)
+    parser.add_argument('--dir', '-d', help='directory with WD models', default='/Users/mmc/lfit/params')
     args = parser.parse_args()
     file = args.file
     thin = args.thin
@@ -225,38 +237,37 @@ if __name__ == "__main__":
         nobjects = (flat*len(fchain))/thin
         fchain = fchain[:nobjects]
     else:
-        #chain = readchain(file)
         chain = readchain_dask(file)
         nwalkers, nsteps, npars = chain.shape
-        fchain = flatchain(chain,npars,thin=thin)
+        fchain = flatchain(chain, npars, thin=thin)
     print("Done!")
 
-    # this is the order of the params in the chain
-    nameList = ['fwd','fdisc','fbs','fd','q','dphi','rdisc','ulimb','rwd','scale', \
-            'az','frac','rexp','off','exp1','exp2','tilt','yaw','amp_gp','tau_gp','lnprob']
+    # grab param names from the file
+    with open(file, 'r') as chain_file:
+        nameList = chain_file.readline().strip().split(' ')[1:]
     # we need q, dphi, rw from the chain
-    qVals = fchain[:,4]
-    dphiVals = fchain[:,5]
-    rwVals  = fchain[:,8]
+    qVals = fchain[:, nameList.index('q_core')]
+    dphiVals = fchain[:, nameList.index('dphi_core')]
+    rwVals = fchain[:, nameList.index('rwd_core')]
     chainLength = len(qVals)
 
     # white dwarf temp
-    twdVals = np.random.normal(loc=args.twd,scale=args.e_twd,size=chainLength)*units.K
+    twdVals = np.random.normal(loc=args.twd, scale=args.e_twd, size=chainLength)*units.K
     # period
-    pVals = np.random.normal(loc=args.p,scale=args.e_p,size=chainLength)*units.d
+    pVals = np.random.normal(loc=args.p, scale=args.e_p, size=chainLength)*units.d
 
     # loop over the MCMC chain, calculating system parameters as we go
 
     # table for results
-    results = Table(names=('q','Mw','Rw','Mr','Rr','a','Kw','Kr','incl'))
+    results = Table(names=('q', 'Mw', 'Rw', 'Mr', 'Rr', 'a', 'Kw', 'Kr', 'incl'))
     # need to be a little careful about astropy versions here, since only
     # versions >=1.0 allow quantities in tables
     # function below extracts value from quantity and floats alike
-    getval = lambda el: getattr(el,'value',el)
+    getval = lambda el: getattr(el, 'value', el)
 
-    psolve = partial(solve,baseDir=baseDir)
-    data = zip(qVals,dphiVals,rwVals,twdVals,pVals)
-    solvedParams = PB.map(psolve,data,multiprocess=True)
+    psolve = partial(solve, baseDir=baseDir)
+    data = zip(qVals, dphiVals, rwVals, twdVals, pVals)
+    solvedParams = PB.map(psolve, data, multiprocess=True)
 
     print('Writing out results...')
     # loop over these results and put all the solutions in our results table
@@ -266,4 +277,4 @@ if __name__ == "__main__":
             results.add_row(thisResult)
 
     print('Found solutions for %d percent of samples in MCMC chain' % (100*float(len(results))/float(chainLength)))
-    results.write('physicalparams.log',format='ascii.commented_header')
+    results.write('physicalparams.log', format='ascii.commented_header')
