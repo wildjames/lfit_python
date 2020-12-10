f01ef1705ca5be471fc4ddbeefdcf21ac25f97ec
wildjames <jwild2@sheffield.ac.uk>
2020-12-10 13:22:22 +0000

    Use the error scale factor when computing the GP

--- plot_lc_model.py
+++ plot_lc_model.py
@@ -223,7 +223,8 @@ def plot_GP_eclipse(ecl_node, save=False, figsize=(11., 8.), fname=None,
     # Create the GP of this eclipse
     gp = ecl_node.create_GP()
     # Compute the GP
-    gp.compute(ecl_node.lc.x)
+    errfact = ecl.ancestor_param_dict['wnoise-fact'].currVal
+    gp.compute(ecl_node.lc.x, ecl_node.lc.ye * errfact)
 
     # Draw samples from the GP
     samples = gp.sample_conditional(residuals, ecl_node.lc.x, size=300)
